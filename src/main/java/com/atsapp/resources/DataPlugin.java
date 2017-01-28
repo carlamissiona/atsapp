afbd4e06237177534ff3ecd71f825ab61d0c690e
devtuna <devtuna@DESKTOP-8JKBEUB>
2017-01-28 15:09:36 +0800

    qwert

--- src/main/java/com/atsapp/resources/DataPlugin.java
+++ src/main/java/com/atsapp/resources/DataPlugin.java
@@ -5,27 +5,28 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Arrays;
 
 public class DataPlugin {
 
-	public boolean connect(){
+	public String connect(){
 		String url ="jdbc:mysql://127.3.148.130:3306/atsapp";
 		String dbuser ="adminiqXUYnu";
 		String dbpass ="en9nhRHBEEbu";
-		boolean result = false;
+		String result = "";
 		try{
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection con = DriverManager.getConnection(url, dbuser,dbpass);
-		    PreparedStatement pst = con.prepareStatement("Select * from Contacts");
+		    PreparedStatement pst = con.prepareStatement("Select * from contacts");
 		    
 		    ResultSet rs = pst.executeQuery();
 		    if(rs.next()){
 		    	
-		    	result = true;
+		    	result = rs.getObject(0).toString() ;
 		    }
 		}catch(Exception e){
 			System.out.println("Hey ");
-	    	  result = false;
+	    	  result = e.getMessage();
 
 		}
 		return result ;
