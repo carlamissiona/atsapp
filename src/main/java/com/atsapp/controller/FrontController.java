7b1414b2510ca73bed24e8df3af8f39441bd0dd7
devtuna <devtuna@DESKTOP-8JKBEUB>
2017-02-15 03:39:50 +0800

    bnbm,m

--- src/main/java/com/atsapp/controller/FrontController.java
+++ src/main/java/com/atsapp/controller/FrontController.java
@@ -99,7 +99,7 @@ public class FrontController {
 		DataPlugin DataPlug = new DataPlugin(); 
 //		cn_firstname=Carla&cn_middlename=A&cn_lastname=Wi&cn_email=missiona.carla%40gmail.com&cn_mobile=09990991&cn_add=qc+qcs&cn_tel=12345&cn_recruiter=1&cn_recruiter=QC&cn_status=interviewed&cn_job_title=Software+Developer
 			//return DataPlug.edit(request.toString() );
-			return request.getParameter("cn_firstname").toString() ;
+			return request.getParameter("cn_firstname") ;
 					
 				
 			  
