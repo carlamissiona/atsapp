fb94c2f3667b6c491cba26b2abf039346ae70a12
devtuna <devtuna@DESKTOP-8JKBEUB>
2017-07-11 23:15:09 +0800

    wqwqw

--- src/main/java/com/atsapp/resources/DataPlugin.java
+++ src/main/java/com/atsapp/resources/DataPlugin.java
@@ -239,26 +239,28 @@ public class DataPlugin {
  
 		 
 		 if(model.contains("users") || model.equalsIgnoreCase("users") || model.contentEquals("users") ){
-			 if( req.getParameter("ac_employement_date").toString().isEmpty() || req.getParameter("ac_employement_date").toString().length() == 0  ){
-				  date_employment = "1990-01-01 00:00:01";
-			 }
-			    sql = "UPDATE accounts SET  ac_firstname='"+ req.getParameter("ac_firstname").toString() +"', "+
-			    		"ac_middlename='"+  req.getParameter("ac_middlename").toString()  +"' , "+
-			    		"ac_lastname='"+  req.getParameter("ac_lastname").toString()  +"' , "+
-			    		"ac_mobile='"+  req.getParameter("ac_mobile").toString()  +"' , "+
-			    		"ac_address='"+  req.getParameter("ac_address").toString()  +"' , "+
-			    		"ac_employement_date ='"+ date_employment  +"' , "+
-			    		"ac_status='"+  req.getParameter("ac_status").toString()  +"' , "+
-			    		"where ac_id ="+ req.getParameter("ac_id").toString(); 
-			    		
-			    		 
-				try{  
-			    	 
-			    	res =  sql;     
-			    }catch(Exception e){
-			    	System.out.println(e);
-			    }  
+//			 if( req.getParameter("ac_employement_date").toString().isEmpty() || req.getParameter("ac_employement_date").toString().length() == 0  ){
+//				  date_employment = "1990-01-01 00:00:01";
+//			 }
+//			    sql = "UPDATE accounts SET  ac_firstname='"+ req.getParameter("ac_firstname").toString() +"', "+
+//			    		"ac_middlename='"+  req.getParameter("ac_middlename").toString()  +"' , "+
+//			    		"ac_lastname='"+  req.getParameter("ac_lastname").toString()  +"' , "+
+//			    		"ac_mobile='"+  req.getParameter("ac_mobile").toString()  +"' , "+
+//			    		"ac_address='"+  req.getParameter("ac_address").toString()  +"' , "+
+//			    		"ac_employement_date ='"+ date_employment  +"' , "+
+//			    		"ac_status='"+  req.getParameter("ac_status").toString()  +"' , "+
+//			    		"where ac_id ="+ req.getParameter("ac_id").toString(); 
+//			    		
+//			    		 
+//				try{  
+//			    	 
+//			    	res =  sql;     
+//			    }catch(Exception e){
+//			    	System.out.println(e);
+//			    }  
 			 
+			 
+			 res= "  bbbbbissssssscth";
 		 }
 		if(model.contains("candidate") || model.equalsIgnoreCase("candidate") || model.contentEquals("candidate") ){
 			 
